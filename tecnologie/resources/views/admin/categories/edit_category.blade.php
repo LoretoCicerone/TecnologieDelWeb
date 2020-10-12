3656e74c026ec392cbee95dc55b9ff9f2c952c8a
nonamepromo <scaruso96@gmail.com>
2020-10-12 21:53:06 +0200

    Vari aggiunte
    Rispetto all'ultimo push ho aggiunto la possibilità di usare il tasto "Settings" nella home del pannello.
    Andando sul menù a sinistra nella sezione categories, si aprirà un sottopannello con add e view, su add sarà possibile aggiungere categorie es, rasoi, lamette eccetera, successivamente si potrà aggiungere una sottocategoria a quest'ultimi ad esempio, rasoi braun dove la marca braun sarà la sotto categoria.
    Su view invece da la possibilità di vedere tutte le categorie e sottocategorie presenti, dove sempre dalla stessa schermata sarà possibile modificarle e /o eliminarle con annesso avviso popup di conferma (da sistemare).
    Fino ad adesso ho un solo problema con l'autoincrement del database riguardo le id delle categorie, poi vi spiegherò meglio ma niente di invasivo.

--- tecnologie/resources/views/admin/categories/edit_category.blade.php
+++ tecnologie/resources/views/admin/categories/edit_category.blade.php
@@ -3,7 +3,7 @@
 
     <div id="content">
         <div id="content-header">
-            <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#">Categories</a> <a href="#" class="current">Add Category</a> </div>
+            <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#">Categories</a> <a href="#" class="current">Edit Category</a> </div>
             <h1>Categories</h1>
         </div>
         <div class="container-fluid"><hr>
@@ -11,14 +11,14 @@
                 <div class="span12">
                     <div class="widget-box">
                         <div class="widget-title"> <span class="icon"> <i class="icon-info-sign"></i> </span>
-                            <h5>Add Category</h5>
+                            <h5>Edit Category</h5>
                         </div>
                         <div class="widget-content nopadding">
-                            <form class="form-horizontal" method="post" action="{{ url('/admin/add-category') }}" name="add_category" id="add_category" novalidate="novalidate"> {{csrf_field()}}
+                            <form class="form-horizontal" method="post" action="{{ url('/admin/edit-category/'.$categoryDetails->id) }}" name="edit_category" id="edit_category" novalidate="novalidate"> {{csrf_field()}}
                                 <div class="control-group">
                                     <label class="control-label">Category Name</label>
                                     <div class="controls">
-                                        <input type="text" name="category_name" id="category_name">
+                                        <input type="text" name="category_name" id="category_name" value="{{ $categoryDetails->name }}">
                                     </div>
                                 </div>
                                 <div class="control-group">
@@ -27,7 +27,7 @@
                                         <select name="parent_id" style="width: 220px;">
                                             <option value="0">Main Category</option>
                                             @foreach($levels as $val)
-                                                <option value="{{ $val->id }}">{{$val->name}}</option>
+                                                <option value="{{ $val->id }}" @if($val->id == $categoryDetails->parent_id) selected @endif>{{$val->name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
@@ -35,17 +35,17 @@
                                 <div class="control-group">
                                     <label class="control-label">Description</label>
                                     <div class="controls">
-                                        <textarea name="description" id="description"></textarea>
+                                        <textarea name="description" id="description">{{ $categoryDetails->name }}</textarea>
                                     </div>
                                 </div>
                                 <div class="control-group">
                                     <label class="control-label">URL</label>
                                     <div class="controls">
-                                        <input type="text" name="url" id="url">
+                                        <input type="text" name="url" id="url" value="{{ $categoryDetails->url }}">
                                     </div>
                                 </div>
                                 <div class="form-actions">
-                                    <input type="submit" value="Add Category" class="btn btn-success">
+                                    <input type="submit" value="Edit Category" class="btn btn-success">
                                 </div>
                             </form>
                         </div>
@@ -54,5 +54,6 @@
             </div>
         </div>
     </div>
+
 
 @endsection
--- tecnologie/routes/web.php
+++ tecnologie/routes/web.php
@@ -27,6 +27,9 @@ Route::group(['middleware'=>['auth']],function () {
 
     // Categorie Routes (Admin)
     Route::match(['get','post'],'/admin/add-category','CategoryController@addCategory');
+    Route::match(['get','post'],'/admin/edit-category/{id}','CategoryController@editCategory');
+    Route::match(['get','post'],'/admin/delete-category/{id}','CategoryController@deleteCategory');
+    Route::get('/admin/view-categories','CategoryController@viewCategories');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
